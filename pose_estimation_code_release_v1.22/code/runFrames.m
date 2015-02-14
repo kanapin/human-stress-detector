ddb559dc6e66d23ee7525359ace70cb9ae3a247b
Nurlan Kanapin <nkanapin@gmail.com>
2015-02-15 02:39:45 +0600

    Added runFrames.m

---
+++ pose_estimation_code_release_v1.22/code/runFrames.m
@@ -0,0 +1,12 @@
+function [] = runFrames(pathToFrameData, fghigh_params, parse_params_Buffy3and4andPascal, pm2segms_params)
+
+fileID = fopen(pathToFrameData, 'r');
+from = fscanf(fileID, 'from=%d ', 1);
+to = fscanf(fileID, 'to=%d\n', 1);
+
+for f = from:to-1
+    my_bb = fscanf(fileID, '[%d %d %d %d]\n', 4);
+    [T sticks_imgcoor] = PoseEstimStillImage(pwd, 'images/frames', '%06d.png', f, 'full', my_bb, fghigh_params, parse_params_Buffy3and4andPascal, [], pm2segms_params, true);
+end
+
+end
